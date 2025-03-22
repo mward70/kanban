01dec7efa19b453bfe05301aa3cee26888c1b51e
Megan Ward <wardmeg1998@gmail.com>
2025-03-21 23:32:45 -0400

    installed dependencies and client side code

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,6 +2,31 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  try {
+    // const { username, password } = userInfo; // Extract username and password
+    const response = await fetch("/auth/login", {  //API endpoint
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInfo }), // Use extracted values
+    });
+
+    if (!response.ok) {
+      console.error("Authentication failed");
+      throw new Error("Invalid login credentials");
+    }
+
+    const data = await response.json();
+    console.log("Logged in with:", data);
+    
+  AuthService.login(data.token);
+    
+    return data;
+  } catch (error) {
+    console.error("Error during authentication:", error);
+    throw error;
+  }
 }
 
 
